e961c082a55a2821086f186dba9e7c882761d432
Padaras <elzbietagrusaite@gmail.com>
2024-06-07 09:28:06 +0300

    API integration and improved JS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,60 @@
-function handleSearchSubmit(event) {
+function formatDate(date) {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+  
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+  
+    let weekday = days[date.getDay()];
+  
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+  
+    string = `${weekday} ${hours}:${minutes}`;
+    return string;
+  }
+  
+  function updateWeatherData(response) {
+    console.log(response);
+    let cityElement = document.querySelector("#city");
+    let temperatureElement = document.querySelector("#current-temperature");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    let date = new Date(response.data.time * 1000);
+    let timeElement = document.querySelector("#time");
+    let iconElement = document.querySelector("#icon");
+  
+    windElement.innerHTML = `${response.data.wind.speed} km/h`;
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    timeElement.innerHTML = formatDate(date);
+    iconElement.innerHTML = `<img src=${response.data.condition.icon_url} class="weather-app-icon" />`;
+  }
+  
+  function searchCity(city) {
+    let apiKey = "obbt00a19b0447e3fbfabf89040f3c88";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(updateWeatherData);
+  }
+  
+  function initiateSearch(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
   }
   
   let searchFormElement = document.querySelector("#search-form");
-  searchFormElement.addEventListener("submit", handleSearchSubmit);
+  searchFormElement.addEventListener("submit", initiateSearch);
+  
+  searchCity("Vilnius");
